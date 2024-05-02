693ae60fadc832ac3845012a77dd2a5a73b8116d
Kehindeee <k.h.amusa@wlv.ac.uk>
2024-05-02 16:21:07 +0100

    added comments for readability and modified the test suite

--- src/components/Spinner.cy.jsx
+++ src/components/Spinner.cy.jsx
@@ -1,9 +1,12 @@
+// Cypress test file for the Spinner component
 import React from 'react'
 import Spinner from './Spinner'
 
+// Test the Spinner component
 describe('<Spinner />', () => {
   it('renders', () => {
     // see: https://on.cypress.io/mounting-react
+    // eslint-disable-next-line no-undef
     cy.mount(<Spinner />)
   })
 })
